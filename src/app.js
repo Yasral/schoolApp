e4e0db4dfb5633b7b5cdfbcfdae39f60e3e2a1f8
Jacques Etienne <santiagondiaye1@gmail.com>
2021-12-24 06:40:50 +0000

    Adding the deletion functionnality

--- src/app.js
+++ src/app.js
@@ -11,12 +11,16 @@ const nom = document.querySelector('input[name="nom"]');
 const prenom = document.querySelector('input[name="prenom"]');
 const niveau = document.querySelector("select");
 const maquette = document.querySelector('input[name="mockup"]');
+const database = document.querySelector('input[name="database"]');
 const userInterface = document.querySelector('input[name="ui"]');
 const composant = document.querySelector('input[name="composant"]');
-const uiStatique = document.querySelector('input[name="ui-statique"]');
-const gestionContenus = document.querySelector('input[name="gestion-contenus"]');
-const backEnd = document.querySelector('input[name="back-end"]');
+const cms = document.querySelector('input[name="cms"]');
+// const uiStatique = document.querySelector('input[name="interfacestatique"]');
+const gestionContenus = document.querySelector('input[name="gestion"]');
+const backEnd = document.querySelector('input[name="backend"]');
 const formulaire = document.querySelector("form");
+
+const cardGroup = document.querySelector(".card-group");
 
 const allValues = [];
 
@@ -63,6 +67,7 @@ formulaire.addEventListener("submit", (e) => {
     if (nom.value.trim().length < 5) {
         nom.classList.remove("green-border");
         nom.classList.add("red-border");
+        return
     } else {
         nom.classList.remove("red-border");
         nom.classList.add("green-border");
@@ -71,6 +76,7 @@ formulaire.addEventListener("submit", (e) => {
     if (prenom.value.trim().length < 3) {
         prenom.classList.remove("green-border");
         prenom.classList.add("red-border");
+        return
     } else {
         prenom.classList.remove("red-border");
         prenom.classList.add("green-border");
@@ -81,13 +87,127 @@ formulaire.addEventListener("submit", (e) => {
         Niveau: niveau.value,
         Biographie: biographie.value,
         Maquette: maquette.value,
+        BaseDonnee: database.value,
         InterfaceUtilisateur: userInterface.value,
         Composant: composant.value,
-        InterfaceStatique: uiStatique.value,
+        Cms: cms.value,
+        // InterfaceStatique: uiStatique.value,
         GestionContenu: gestionContenus.value,
         BackEnd: backEnd.value
     }
 
-    allValues.push(formValues);
-    console.log(allValues);    
-})  
+    formulaire.reset();
+
+    // allValues.push(formValues);
+    displayCards(formValues); 
+}) 
+
+// Handling the display of the card content 
+
+let identifiant = 1;
+
+let displayCards = (someContent) =>{
+    // let formInput = [];
+    // let divContent = ""
+    // someContent.forEach((content) =>{
+        cardGroup.insertAdjacentHTML("afterbegin", `
+            <div class="card" data-id="${identifiant}">
+                <div class="card-header"></div>
+    
+                     <div class="body-content">
+                        <div class="body-img">
+                            <img src="" alt="Picture" class="avatar">
+                        </div>
+                        <div class="content">
+                            <h3 class="fullName"><span>${someContent.Nom}</span> <span>${someContent.Prenom}</span></h3>
+                            <i class="far fa-edit"></i>
+                            <i class="far fa-trash-alt"></i>
+                        </div>
+                    </div>
+                        <p class="biographie-description">
+                            ${someContent.Biographie}
+                        </p>
+                            
+                        <div class="card-footer">
+                            <p class="card-level">
+                                ${someContent.Niveau}
+                            </p>
+                        </div>
+                    </div>     
+    
+        `)
+        identifiant++
+    // })
+
+    // Deleting the values
+    const deleteBtn = document.querySelector(".fa-trash-alt");
+    deleteBtn.addEventListener("click", removeCard);
+
+    // Updating the values
+    const updateBtn = document.querySelector(".fa-edit");
+    updateBtn.addEventListener("click", (e) => {
+        updateCard(someContent)
+        
+    });
+}
+
+// Removing a card
+let removeCard = (e) =>{
+    e.target.parentElement.parentElement.parentElement.remove()
+}
+
+// Updating a card
+const updateBtn = document.querySelector(".fa-edit");
+
+let updateCard = (valeur) =>{
+    formulaire.nom.value = valeur.Nom;
+    formulaire.prenom.value = valeur.Prenom;
+    // Have to Handle the select
+    formulaire.bio.value = valeur.Biographie;
+    formulaire.mockup.value = valeur.Maquette;
+    formulaire.database.value = valeur.BaseDonnee;
+    formulaire.ui.value = valeur.InterfaceUtilisateur;
+    formulaire.composant.value = valeur.Composant;
+    formulaire.cms.value = valeur.Cms;
+    // formulaire.interfacestatique.value = valeur.InterfaceStatique;
+    formulaire.gestion.value = valeur.GestionContenu;
+    formulaire.backend.value = valeur.BackEnd;
+    
+    // Getting the form and updating the values
+    // formulaire.addEventListener("submit", (e) =>{
+
+    //     e.preventDefault();
+
+    //     if (nom.value.trim().length < 5) {
+    //         nom.classList.remove("green-border");
+    //         nom.classList.add("red-border");
+    //         return
+    //     } else {
+    //         nom.classList.remove("red-border");
+    //         nom.classList.add("green-border");
+    //     }
+
+    //     if (prenom.value.trim().length < 3) {
+    //         prenom.classList.remove("green-border");
+    //         prenom.classList.add("red-border");
+    //         return
+    //     } else {
+    //         prenom.classList.remove("red-border");
+    //         prenom.classList.add("green-border");
+    //     }
+    //     const formValues = {
+    //         Nom: nom.value,
+    //         Prenom: prenom.value,
+    //         Niveau: niveau.value,
+    //         Biographie: biographie.value,
+    //         Maquette: maquette.value,
+    //         BaseDonnee: database.value,
+    //         InterfaceUtilisateur: userInterface.value,
+    //         Composant: composant.value,
+    //         Cms: cms.value,
+    //         // InterfaceStatique: uiStatique.value,
+    //         GestionContenu: gestionContenus.value,
+    //         BackEnd: backEnd.value
+    //     }  
+    // })
+}
